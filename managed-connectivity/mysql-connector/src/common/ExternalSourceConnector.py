c3cfa1bdfa62781c0a4819c3d10bf9ed6f6c09e4
danielholgate <dholgate@gmail.com>
2025-03-16 01:22:43 +0100

    experimental mysql

--- managed-connectivity/mysql-connector/src/common/ExternalSourceConnector.py
+++ managed-connectivity/mysql-connector/src/common/ExternalSourceConnector.py
@@ -3,12 +3,12 @@ from src.constants import EntryType
 from pyspark.sql import DataFrame
 from abc import ABC, abstractmethod
 
-# Interface defines methos to pass metadata to common files
+# Interface defines methods for connector to pass metadata to common connector logic
 class ExternalSourceConnector(ABC):
 
     @abstractmethod
     def get_dataset(self, schema_name: str, entry_type: EntryType):
-        """Returns dataframe of schemas to extract objects from"""
+        """Returns db object data for given schema"""
         pass
 
     @abstractmethod
