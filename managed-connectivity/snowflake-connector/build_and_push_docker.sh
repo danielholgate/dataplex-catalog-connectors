f7dc507e58e5cc468bf632d351503aba071113ee
danielholgate <dholgate@gmail.com>
2025-04-09 13:53:11 +0200

    snowflake docker

--- managed-connectivity/snowflake-connector/build_and_push_docker.sh
+++ managed-connectivity/snowflake-connector/build_and_push_docker.sh
@@ -12,7 +12,7 @@ REPO_IMAGE=${REGION}-docker.pkg.dev/${PROJECT_ID}/docker-repo/${IMAGE_NAME}
 docker build -t "${IMAGE}" .
 
 # Tag and push to GCP container registry
-gcloud config set project ${PROJECT}
+gcloud config set project ${PROJECT_ID}
 gcloud auth configure-docker ${REGION}-docker.pkg.dev
 docker tag "${IMAGE}" "${REPO_IMAGE}"
 docker push "${REPO_IMAGE}"
